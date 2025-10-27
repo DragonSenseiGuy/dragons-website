86020835d561aec1592bcb936f26b16e861c2773
DragonSenseiGuy <200907890+DragonSenseiGuy@users.noreply.github.com>
2025-10-27 17:25:46 -0400

    Fix build script to use python3

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@
 set -o errexit
 
 # Install dependencies
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Apply database migrations
-python manage.py migrate
+python3 manage.py migrate
